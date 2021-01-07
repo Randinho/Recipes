c0c5a6d8779d532fe89b1683566bf484d6c1b10d
Kamil Płaskociński <kamilo.plastik@outlook.com>
2021-01-07 13:19:51 +0100

    generating qrcode with shopping list of recipe's ingredients

---
+++ Recipes/Controllers/ShoppingListController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Recipes.Data;
+using Recipes.Models;
+using Recipes.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using QRCoder;
+using System.Drawing;
+using System.IO;
+
+namespace Recipes.Controllers
+{
+    public class ShoppingListController : BaseController
+    {
+        private readonly ApplicationDbContext context;
+        private readonly ILogger<Ingredient> logger;
+        public ShoppingListController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, ILogger<Ingredient> logger) : base(userManager)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+
+        public async Task<IActionResult> Index(int recipeId)
+        {
+            List<ShoppingListItemViewModel> shoppingList = new List<ShoppingListItemViewModel>();
+            var recipe = await context.Recipes
+                .Include(x => x.RecipeIngredients)
+                .ThenInclude(ri => ri.Ingredient)
+                .FirstOrDefaultAsync(x => x.Id == recipeId);
+
+            foreach(var ingredient in recipe.RecipeIngredients.ToList())
+            {
+                shoppingList.Add(new ShoppingListItemViewModel
+                {
+                    Id = ingredient.IngredientId,
+                    Name = ingredient.Ingredient.Name,
+                    InPossession = false
+                });
+            }
+            
+            return View(shoppingList);
+        }
+
+        [HttpPost]
+        public IActionResult Create(List<ShoppingListItemViewModel> shoppingList)
+        {
+            shoppingList = shoppingList.Where(x => x.InPossession == false).ToList();
+            string txt = String.Join("\n", shoppingList.Select(s => s.Name).ToArray());
+            TempData["QRCodeText"] = txt;
+            return RedirectToAction(nameof(ShoppingListCode));
+        }
+
+        
+        public IActionResult ShoppingListCode()
+        {
+            var txt = TempData["QRCodeText"] as string;
+
+            QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrCodeGenerator.CreateQrCode(txt,
+                QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+
+            return View(BitmapToBytes(qrCodeImage));
+        }
+
+        [NonAction]
+        private static Byte[] BitmapToBytes(Bitmap image)
+        {
+            using(MemoryStream stream = new MemoryStream())
+            {
+                
+                image.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
+}
---
+++ Recipes/ViewModels/ShoppingListItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipes.ViewModels
+{
+    public class ShoppingListItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool InPossession { get; set; }
+    }
+}
